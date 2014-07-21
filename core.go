1f2205f851b2995cb87f7aff99daefea228e1e04
obsc <Abscuron@gmail.com>
2014-07-20 22:40:21 -0700

    added monadic binding

--- core.go
+++ core.go
@@ -13,14 +13,38 @@ type deffered struct {
 func Deffered(f interface{}) *deffered {
 	def := &deffered{nil, false, make(chan bool)}
 	go func() {
-		ff := reflect.ValueOf(f)
-		def.ret = ff.Call(nil)
+		ffun := reflect.ValueOf(f)
+		def.ret = ffun.Call(nil)
 		def.done = true
 		close(def.wait)
 	}()
 	return def
 }
 
+func Init(v ...interface{}) *deffered {
+	def := &deffered{make([]reflect.Value, len(v)), true, make(chan bool)}
+	for i := range v {
+		def.ret[i] = reflect.ValueOf(v[i])
+	}
+	close(def.wait)
+	return def
+}
+
 func (def *deffered) IsDone() bool {
 	return def.done
+}
+
+func (def *deffered) Bind(f interface{}) *deffered {
+	newdef := &deffered{nil, false, make(chan bool)}
+	go func() {
+		for _ = range def.wait {
+		}
+
+		ffun := reflect.ValueOf(f)
+		ftyp := reflect.TypeOf(f)
+		newdef.ret = ffun.Call(def.ret[0:ftyp.NumIn()])
+		newdef.done = true
+		close(newdef.wait)
+	}()
+	return newdef
 }
